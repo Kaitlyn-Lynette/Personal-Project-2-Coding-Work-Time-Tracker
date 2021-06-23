e9bd802eabd0d1e45a2f6c9553c98bc620ad6f5f
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2021-06-23 17:59:32 -0400

    Added a text area box

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-// <!-- Timeblocks go here -->
-
-// <!-- GIVEN I am using a daily planner to create a schedule
+// <!-- The purpose of this planner is to track time. 
 
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
 
 // Go to browser and fetch the date
-
 var todayDate = moment().format("MMM Do YYYY");
 $("#currentDay").html(todayDate);
 console.log(todayDate);
 // WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
 
-//Creates a a row for each day 
+// THEN I am presented with 6 days of the week
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 days.map((day) => {
-//This is the text box for the day of the week 
+//This is the row for the day of the week 
   var dayRow = $("<div>");
   dayRow.attr("class", "row day");
+  // $(".row").attr("contenteditable", "true");
+  $(".row").append('<textarea>')
 
-//This is the day of the week 
+//This is the div for the of the week 
   var dayOfWeek = $("<div>")
   dayOfWeek.attr("class", "week day");
-  dayOfWeek.text(day)
+  dayOfWeek.text(day);
 
-  // var daySpan = $("<span>");
-  // daySpan.attr("class","hour col")
-  // daySpan.text(day)
-  // dayOfWeek.append(daySpan)
-
+//This is the button div for each row
   var buttonDiv = $("<div>")
   $(".time-block").append(buttonDiv);
-  
 
-  // var saveBtn = $("<button>").text("save");
-  // saveBtn.attr("class", "saveBtn");
-  // saveBtn.text("Save");
- 
- 
-  // dayRow.append(saveBtn);
+
+//This is where I append the divs and button on the button div
   $(".time-block").append('<button class="saveBtn">Save</button>') 
   $(".time-block").append(dayRow);
   $(".time-block").append(dayOfWeek);
+
+  // saveBtn.on("click", function() {
+  //   var event = timeStamp.val();
+  //   localStorage.setItem(day.stringTime, event);
+  // })
+
+ 
 });
